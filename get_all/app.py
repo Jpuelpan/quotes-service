bd7604dacf1d060c764d3fc54e1750ba62b53aea
Juan Puelpan <jpuelpan528@gmail.com>
2019-12-28 02:23:04 -0300

    Get all quotes from table

--- get_all/app.py
+++ get_all/app.py
@@ -4,10 +4,24 @@ import boto3
 
 QUOTES_TABLE = os.getenv('QUOTES_TABLE')
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(QUOTES_TABLE)
+
+def parse_items(item):
+    item['createdAt'] = int(item['createdAt'])
+    return item
+
 def lambda_handler(event, context):
+    result = table.scan(
+        Limit=25
+    )
+
+    body = {
+        'quotes': list(map(parse_items, result['Items']))
+    }
+
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world "
-        }),
+        "body": json.dumps(body),
    }
+
